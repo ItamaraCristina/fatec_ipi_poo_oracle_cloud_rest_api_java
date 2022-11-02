cdc744286bea49383c36563921b3eb8cdbb26a80
ItamaraCristina <itamara_1245@hotmail.com>
2022-11-02 16:28:52 -0300

    apostila01(oracle_cloud): bloco de codigo 2.10.1. exibindo apena nome, sem iteracao sobre a colecao

--- src/App.java
+++ src/App.java
@@ -10,3 +10,6 @@ public class App {
         service.listar();
     }
 } 
+
+
+
--- src/PessoaService.java
+++ src/PessoaService.java
@@ -2,6 +2,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
+
+import org.json.*;
+import org.json.JSONObject;
+
 import lombok.RequiredArgsConstructor;
 
 
@@ -15,6 +19,12 @@ public class PessoaService {
      HttpRequest request = HttpRequest.newBuilder().uri(URI.create(url)).build();
         var response = client.send(request, BodyHandlers.ofString());
         System.out.println(response.body());
+        JSONObject raiz = new JSONObject(response.body());
+        JSONArray items = raiz.getJSONArray("items");
+        JSONObject primeiro = items.getJSONObject(0);
+        System.out.println(primeiro);
+        String nome = primeiro.getString("nome");
+        System.out.println(nome);
 
     }
     catch(Exception e){
@@ -28,3 +38,4 @@ public class PessoaService {
 
 //objeto exception e que te entrega informação
 //https://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@github.com/ItamaraCristina/fatec_ipi_poo_oracle_cloud_rest_api_java.git
+// 
